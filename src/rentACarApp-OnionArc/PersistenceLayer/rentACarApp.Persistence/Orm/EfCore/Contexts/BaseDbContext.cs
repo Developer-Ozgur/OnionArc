4b06bd090eb590c6d9dac9665d489909050bc2c0
1OZGUR1 <ozguratilgan79@gmail.com>
2022-09-06 02:49:30 +0300

    002.Persistence : Create BaseDbContext Class

---
+++ src/rentACarApp-OnionArc/PersistenceLayer/rentACarApp.Persistence/Orm/EfCore/Contexts/BaseDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace rentACarApp.Persistence.Orm.EfCore.Contexts;
+
+#region Description
+
+//TODO 002.Persistence : Create BaseDbContext Class
+
+#endregion
+public class BaseDbContext : DbContext
+{
+
+}
