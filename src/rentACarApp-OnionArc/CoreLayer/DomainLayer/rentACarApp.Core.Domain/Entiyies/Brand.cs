3b4e751eb993d6e75aa0182df5ee83300cef4fee
1OZGUR1 <ozguratilgan79@gmail.com>
2022-09-06 02:44:43 +0300

    001.Domain : Create Brand Entity Class

---
+++ src/rentACarApp-OnionArc/CoreLayer/DomainLayer/rentACarApp.Core.Domain/Entiyies/Brand.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Repositories;
+
+namespace rentACarApp.Core.Domain.Entiyies;
+
+#region Description
+
+//TODO 001.Domain : Create Brand Entity Class
+
+#endregion
+public class Brand:Entity
+{
+    public string Name { get; set; }
+
+    public Brand()
+    {
+    }
+
+    public Brand(int id, string name) : this()
+    {
+        Id = id;
+        Name = name;
+    }
+}
